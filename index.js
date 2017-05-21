dc7d333ba28551de629f460b2e4bdfe1b658d0fe
lcb931023 <lcb931023@gmail.com>
2017-05-21 00:33:28 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const randomColor = require('randomcolor');
+
+global.THREE = require('three');
+const TWEEN = require('tween.js');
+const createOrbitViewer = require('three-orbit-viewer')(THREE)
+
+/* Scene */
+var app = createOrbitViewer({
+  clearColor: 0x000000,
+  clearAlpha: 1,
+  fov: 65,
+  position: new THREE.Vector3(0, 0, 1), // camera
+})
+
+app.on('tick', function(dt) {
+  //.. handle pre-render updates
+  TWEEN.update();
+})
+
